b84801a74086fe1424a0b41c4a0d72c007c52f57
Anastasia Kunitskaia <kunitskaiaanastasia@gmail.com>
2023-09-02 16:25:53 +0300

    step 2 add empty default chat page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,12 +2,13 @@ import '../App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import NotFoundPage from './NotFoundPage';
+import ChatPage from './ChatPage';
 
 const App = () => (
     <Router>
         <Routes>
-          <Route path="/" element={<App/> } />
-          <Route path="one" element={<LoginPage />} />
+          <Route path="/" element={<ChatPage/> } />
+          <Route path="login" element={<LoginPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
     </Router>
---
+++ frontend/src/components/ChatPage.jsx
@@ -0,0 +1,9 @@
+
+const ChatPage = () => {
+
+    return (
+        <p>chat page </p>
+    )
+}
+
+export default ChatPage;
